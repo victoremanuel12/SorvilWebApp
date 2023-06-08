4048c70b3432b78ad690b6f198e6bf9900ae7fd5
Victor E. Silva <victor.silva@renova.com.br>
2023-06-08 00:27:10 -0300

    First Commit

---
+++ BookStore/Controllers/UserAccount.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class UserAccount : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookStore/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O nome deve ser informado")]
+        [MinLength(3, ErrorMessage = "O nome deve conter mínimo 3 caracteres")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo Email deve ser um endereço de email válido.")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "O campo Email deve ser um endereço de email válido.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Preencha a senha, com no minimo 4 caracteres")]
+        [StringLength(12, MinimumLength = 4)]
+        [DataType(DataType.Password)]
+        public int Password { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
