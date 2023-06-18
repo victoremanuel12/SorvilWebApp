146be7c3ec9ac19cbfa0f10bd8dd0972c39e4f0e
Victoremanuel12 <Victoremannuel12@hotmail.com>
2023-06-18 18:44:29 -0300

    Adicao de injecao na home controller

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -7,6 +7,14 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly RequestApiBook _requestApiBook;
+
+        public HomeController(RequestApiBook requestApiBook)
+        {
+            _requestApiBook = requestApiBook;
+        }
+
         public IActionResult Index()
         {
             return View();
