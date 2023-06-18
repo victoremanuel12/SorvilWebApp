1d39141b8d7f80dc12c6a841865560ba3b7abf88
Gabriel Pontes <gabriel.pontes@suporterenova.onmicrosoft.com>
2023-06-18 18:06:49 -0300

    Tela de apresentação

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -7,14 +7,6 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
-
-        private readonly RequestApiBook _requestApiBook;
-
-        public HomeController(RequestApiBook requestApiBook)
-        {
-            _requestApiBook = requestApiBook;
-        }
-
         public IActionResult Index()
         {
             return View();
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -43,6 +43,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Apresentacao}/{action=Index}/{id?}");
 
 app.Run();
