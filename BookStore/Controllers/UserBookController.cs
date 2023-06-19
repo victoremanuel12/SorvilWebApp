6db9f752a61d85fab0b10213f7a87458155c7eb2
Gabriel Pontes <gabriel.pontes@suporterenova.onmicrosoft.com>
2023-06-19 16:37:20 -0300

    Correção de conflitos no merge das branches

--- BookStore/Controllers/UserBookController.cs
+++ BookStore/Controllers/UserBookController.cs
@@ -24,7 +24,6 @@ namespace BookStore.Controllers
             try
             {
                 var book = _requestApiBook.GetBookById(bookId);
-                string Authors = "";
                 if (book.Result == null)
                 {
                     TempData["Error"] = "Não foi possivel adicionar esse livro a sua prateleira";
@@ -33,29 +32,27 @@ namespace BookStore.Controllers
                 }
                 else
                 {
-                    foreach (var authors in book.Result.VolumeInfo.Authors)
-                    {
-                        Authors += "/" + authors;
-                    }
-                    //User user = await _uow.UserRepository.GetById(ClienteInSession.GetClienteFromSession(HttpContext));
-                    User user = await _uow.UserRepository.GetById(7);
+
+                    User userInSession = ClienteInSession.GetClienteFromSession(HttpContext);
+                    User user = await _uow.UserRepository.GetById(userInSession.Id);
                     var UserBook = new UserBook
                     {
                         BookId = book.Result.Id,
-                        Author = Authors,
+                        Author = string.Join("/", book.Result.VolumeInfo.Authors),
                         Description = book.Result.VolumeInfo.Description,
                         Image = book.Result.VolumeInfo.ImageLinks.Thumbnail,
                         IndustryIdentifier = book.Result.VolumeInfo.IndustryIdentifiers.ToString(),
                         Title = book.Result.VolumeInfo.Title,
                         PageCount = book.Result.VolumeInfo.PageCount,
                         PublishedDate = book.Result.VolumeInfo.PublishedDate,
-                        UserId = 7, //user.Id
-                        User = user, //user
-                        Status = (BookStatus)bookStatus
+                        UserId = user.Id,
+                        User = user,
+                        Status = (BookStatus)bookStatus,
+
                     };
                     _uow.UserBookRepository.Add(UserBook);
                     await _uow.Commit();
-                    TempData["Success"] = "Não foi possivel adicionar esse livro a sua prateleira";
+                    TempData["Success"] = $"O livro {UserBook.Title} foi adicionado a sua prateleira";
                     return RedirectToAction("Index", "Home");
 
                 };
