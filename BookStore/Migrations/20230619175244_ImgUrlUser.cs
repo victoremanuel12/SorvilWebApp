0f49e418b63f86c4e90ce86de458c951196abd12
Victoremanuel12 <Victoremannuel12@hotmail.com>
2023-06-19 19:54:27 -0300

    caralhop orar

---
+++ BookStore/Migrations/20230619175244_ImgUrlUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImgUrlUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagemUsuario",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagemUsuario",
+                table: "Users");
+        }
+    }
+}
